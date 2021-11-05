7558395d1992b6f205d031b86a0c9874a7eb054c
German Arevalo J <germanarevalojerez@gmail.com>
2021-11-05 11:48:43 -0500

    google ID Token

---
+++ helpers/google-verify-token.js
@@ -0,0 +1,31 @@
+const { OAuth2Client } = require("google-auth-library");
+
+const CLIENT_ID =
+  "319782720719-mqlq4krcrb9lo9upnmesg937o7hnr90t.apps.googleusercontent.com";
+
+const client = new OAuth2Client(CLIENT_ID);
+
+const validarGoogleIdToken = async (token) => {
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: [
+        CLIENT_ID,
+        "319782720719-6if378f3fn0oh7si9d8sj1eg3h2r3al3.apps.googleusercontent.com",
+      ],
+    });
+    const payload = ticket.getPayload();
+
+    return {
+      name: payload["name"],
+      picture: payload["picture"],
+      email: payload["email"],
+    };
+  } catch (error) {
+    return null;
+  }
+};
+
+// validarGoogleIdToken().catch(console.error);
+
+module.exports = { validarGoogleIdToken };
